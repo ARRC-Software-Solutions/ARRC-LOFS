aa654b663fbc85e3d1a4a7ab48dded1e201e26f3
Kyrilios <Cyriljohnamoronio@outlook.com>
2020-02-25 22:26:52 +0800

    initial commit

---
+++ ARRCLogin/index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Login</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<div class="login">
+        <center>
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+		<img class="mcmLogo" src="assets/mcmLogo.png" alt="MCM Logo" height = "150px">
+		<img class="mcmLogo" src="assets/ARRC-A.png" alt="MCM Logo" height = "150px">
+        </center>
+			<form action="authenticate.php" method="post">
+				<p> <? $_SESSION['message']?> </p>
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label  >
+				<input type="text" name="username" placeholder="Username" id="username" >
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Password" id="password" >
+				<input type="submit" value="Login">
+			</form>
+		</div>
+	</body>
+</html>
